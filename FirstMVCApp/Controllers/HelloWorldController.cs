8fc19d82fede53ac1a5a38dd36d8da89abdb856c
mugokevin <kevinmugo4@gmail.com>
2022-11-18 13:07:08 +0300

    initial commit

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,34 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            //DogViewModel doggo = new DogViewModel() { Name="kaigo", Age= 10};
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            //return View("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Hello()
+        {
+            return "Who is there";
+        }
+    }
+}
